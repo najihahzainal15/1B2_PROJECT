418f491489e103494febe0d6b6dea0ec19c3cec7
hdy4hhzn <163952132+hdy4hhzn@users.noreply.github.com>
2025-06-09 14:49:48 +0800

    Update s_committeeAppView.php

--- s_committeeAppView.php
+++ s_committeeAppView.php
@@ -316,7 +316,7 @@
   $servername = "localhost";
   $username = "root"; // Your database username
   $password = "";     // Your database password
-  $dbname = "your_database_name"; // Replace with your actual database name
+  $dbname = "laravel"; // Replace with your actual database name
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
